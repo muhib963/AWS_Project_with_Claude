ee15b23ee22066ba7e45f9873ab431be9e815d29
muhib963 <muhib963@gmail.com>
2025-06-24 23:32:41 -0600

    claude.py

--- claude.py
+++ claude.py
@@ -1,20 +1,24 @@
 import boto3
 import json
 
-prompt_data="""
-Act as a Shakespeare and write a poem on Genertaive AI
-"""
+prompt_data="write a poem on Genertaive AI"
 
 bedrock=boto3.client(service_name="bedrock-runtime")
 
 payload={
-    "prompt":prompt_data,
-    "maxTokens":512,
-    "temperature":0.8,
-    "topP":0.8
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 512,
+    "temperature": 0.5,
+    "messages": [
+        {
+            "role": "user",
+            "content": [{"type": "text", "text": prompt_data}],
+        }
+    ],
 }
+
 body = json.dumps(payload)
-model_id = "ai21.j2-mid-v1"
+model_id = "anthropic.claude-3-haiku-20240307-v1:0"  # Replace with your model ID
 response = bedrock.invoke_model(
     body=body,
     modelId=model_id,
@@ -23,5 +27,7 @@ response = bedrock.invoke_model(
 )
 
 response_body = json.loads(response.get("body").read())
-response_text = response_body.get("completions")[0].get("data").get("text")
+response_text = response_body.get("content")[0].get("text")
 print(response_text)
+
+
